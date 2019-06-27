ad4ea081397888b1d639869b45bf11869311d926
yongyang.wu <yongyang.wu@outlook.com>
2019-06-27 19:48:50 +0800

    1

--- 13.JavaScript_跨域大全篇/1.直接本地html请求后端数据-CORS跨域/1.CROS跨域/get.js
+++ 13.JavaScript_跨域大全篇/1.直接本地html请求后端数据-CORS跨域/1.CROS跨域/get.js
@@ -2,7 +2,7 @@
 var http = require("http");
 var port = 3000;
 http.createServer(function (req,res) {
-    res.setHeader("Access-Control-Allow-Origin","*");//或者下面的也可以；
+    // res.setHeader("Access-Control-Allow-Origin","*");//或者下面的也可以；
     // res.writeHead(200,{"Access-Control-Allow-Origin":"*"});
     res.end("from server data")
 }).listen(port,"127.0.0.1",function () {
